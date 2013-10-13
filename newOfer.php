a37ff56eaa8c717655c67064510a3918aada40d2
thamjan <thamjan@yahoo.com>
2013-10-13 17:18:32 +0200

    work, work

--- newOfer.php
+++ newOfer.php
@@ -2,6 +2,31 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title></title>
+        <script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
+        <script>
+            $(document).ready(function() {
+//                alert('$(document).ready(function(){');
+                $slOfferType = $('#slOfferType');
+                $sCargo = $('#sCargo');
+                $sTruck = $('#sTruck');
+
+                $sCargo.slideUp(1);
+                $sTruck.slideUp(1);
+
+                $slOfferType.change(function() {
+                    if ($slOfferType.val() === 'cargo') {
+//                        alert('cargo');
+                        $sCargo.slideDown('slow');
+                        $sTruck.slideUp('slow');
+                    }
+                    else if ($slOfferType.val() === 'truck') {
+//                        alert('truck');
+                        $sCargo.slideUp('slow');
+                        $sTruck.slideDown('slow');
+                    }
+                });
+            });
+        </script>
     </head>
     <body>
         <h2>Nova ponuda robe</h2>
@@ -19,76 +44,153 @@
                         </select>
                     </td>
                 </tr>
-                <tr>
-                    <td>
-                        <label>Početna adresa</label>
-                    </td>
-                    <td>
-                        <input type="text" id="txtStartAdress" />
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <label>Odredišna adresa</label>
-                    </td>
-                    <td>
-                        <input type="text" id="txtDestinationAdress" />
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <label>Dimenzije</label>
-                    </td>
-                    <td>
-                        <label>Dužina</label>
-                        <input type="text" id="txtLength" />
-                        <label>Širina</label>
-                        <input type="text" id="txtWidth" />
-                        <label>Visina</label>
-                        <input type="text" id="txtHeight" />
-                    </td>
-                </tr>
-                <label>Početna adresa</label>
-                <tr>
-                    <td>
-                        <label>Tip/materijal robe</label>
-                    </td>
-                    <td>
-                        <select id="slCartgoType">
-                            <option value=""></option>
-                            <option value="type1">Tip 1</option>
-                            <option value="type2">Tip 2</option>
-                        </select>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <label>Slike</label>
-                    </td>
-                    <td>
-                        <input type="file" id='filePhotos' />
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                    </td>
-                    <td>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                    </td>
-                    <td>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                    </td>
-                    <td>
-                    </td>
-                </tr>
-                <input type="text" id="txtStartAdress" />
             </table>
+            <section id='sCargo'>
+                <table>
+                    <tr>
+                        <td>
+                            <label>Početna adresa</label>
+                        </td>
+                        <td>
+                            <input type="text" id="txtStartAdress" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label>Odredišna adresa</label>
+                        </td>
+                        <td>
+                            <input type="text" id="txtDestinationAdress" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label>Dimenzije</label>
+                        </td>
+                        <td>
+                            <label>Dužina</label>
+                            <input type="text" id="txtLength" />
+                            <label>Širina</label>
+                            <input type="text" id="txtWidth" />
+                            <label>Visina</label>
+                            <input type="text" id="txtHeight" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label>Tip/materijal robe</label>
+                        </td>
+                        <td>
+                            <select id="slCartgoType">
+                                <option value=""></option>
+                                <option value="type1">Tip 1</option>
+                                <option value="type2">Tip 2</option>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label>Slike</label>
+                        </td>
+                        <td>
+                            <input type="file" id='filePhotos' />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label>Budžet</label>
+                        </td>
+                        <td>
+                            <input type="text" id="txtBudget" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label>Napomene</label>
+                        </td>
+                        <td>
+                            <textarea id='taRemarks' cols="66" rows="6">
+                        
+                            </textarea>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                        </td>
+                        <td>
+                        </td>
+                    </tr>
+                </table>
+            </section>
+            <section id='sTruck'>
+                <table>
+                    <tr>
+                        <td>
+                            <label>Trenutna lokacija</label>
+                        </td>
+                        <td>
+                            <input type="text" id="txtCurrentLocation" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label>Tip vozila</label>
+                        </td>
+                        <td>
+                            <select id="slTruckType">
+                                <option value=""></option>
+                                <option value="type1">Tip 1</option>
+                                <option value="type2">Tip 2</option>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label>Dimenzije</label>
+                        </td>
+                        <td>
+                            <label>Dužina</label>
+                            <input type="text" id="txtLength" />
+                            <label>Širina</label>
+                            <input type="text" id="txtWidth" />
+                            <label>Visina</label>
+                            <input type="text" id="txtHeight" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label>Nosivost</label>
+                        </td>
+                        <td>
+                            <input type="text" id="txtCapacity" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label>Slike</label>
+                        </td>
+                        <td>
+                            <input type="file" id='filePhotos' />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label>Napomene</label>
+                        </td>
+                        <td>
+                            <textarea id='taRemarks' cols="66" rows="6">
+                        
+                            </textarea>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                        </td>
+                        <td>
+                        </td>
+                    </tr>
+                </table>
+            </section>
         </form>
         <?php
         // put your code here
